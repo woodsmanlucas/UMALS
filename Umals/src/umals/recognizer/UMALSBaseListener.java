f69a9bbc85d548916ce072ba05eef770f3c2a359
Declan Easton <declaneaston@gmail.com>
2015-03-25 21:07:00 -0700

    UMALSGrammar is the Lua grammar from the example antlr grammars.  The generated files are in umals.recognizer

---
+++ Umals/src/umals/recognizer/UMALSBaseListener.java
@@ -0,0 +1,448 @@
+// Generated from C:\Users\Declan Easton\Documents\Development\UMALS\Umals\src\UMALSGrammar\UMALS.g4 by ANTLR 4.4
+package umals.recognizer;
+
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.misc.NotNull;
+import org.antlr.v4.runtime.tree.ErrorNode;
+import org.antlr.v4.runtime.tree.TerminalNode;
+
+/**
+ * This class provides an empty implementation of {@link UMALSListener},
+ * which can be extended to create a listener which only needs to handle a subset
+ * of the available methods.
+ */
+public class UMALSBaseListener implements UMALSListener {
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterOperatorUnary(@NotNull UMALSParser.OperatorUnaryContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitOperatorUnary(@NotNull UMALSParser.OperatorUnaryContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterFuncname(@NotNull UMALSParser.FuncnameContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitFuncname(@NotNull UMALSParser.FuncnameContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterOperatorAnd(@NotNull UMALSParser.OperatorAndContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitOperatorAnd(@NotNull UMALSParser.OperatorAndContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterFieldsep(@NotNull UMALSParser.FieldsepContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitFieldsep(@NotNull UMALSParser.FieldsepContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterString(@NotNull UMALSParser.StringContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitString(@NotNull UMALSParser.StringContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterFunctioncall(@NotNull UMALSParser.FunctioncallContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitFunctioncall(@NotNull UMALSParser.FunctioncallContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterParlist(@NotNull UMALSParser.ParlistContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitParlist(@NotNull UMALSParser.ParlistContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterChunk(@NotNull UMALSParser.ChunkContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitChunk(@NotNull UMALSParser.ChunkContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterExplist(@NotNull UMALSParser.ExplistContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitExplist(@NotNull UMALSParser.ExplistContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterRetstat(@NotNull UMALSParser.RetstatContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitRetstat(@NotNull UMALSParser.RetstatContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterVarOrExp(@NotNull UMALSParser.VarOrExpContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitVarOrExp(@NotNull UMALSParser.VarOrExpContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterNumber(@NotNull UMALSParser.NumberContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitNumber(@NotNull UMALSParser.NumberContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterPrefixexp(@NotNull UMALSParser.PrefixexpContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitPrefixexp(@NotNull UMALSParser.PrefixexpContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterNameAndArgs(@NotNull UMALSParser.NameAndArgsContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitNameAndArgs(@NotNull UMALSParser.NameAndArgsContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterNamelist(@NotNull UMALSParser.NamelistContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitNamelist(@NotNull UMALSParser.NamelistContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterFunctiondef(@NotNull UMALSParser.FunctiondefContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitFunctiondef(@NotNull UMALSParser.FunctiondefContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterBlock(@NotNull UMALSParser.BlockContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitBlock(@NotNull UMALSParser.BlockContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterOperatorComparison(@NotNull UMALSParser.OperatorComparisonContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitOperatorComparison(@NotNull UMALSParser.OperatorComparisonContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterVarlist(@NotNull UMALSParser.VarlistContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitVarlist(@NotNull UMALSParser.VarlistContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterExp(@NotNull UMALSParser.ExpContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitExp(@NotNull UMALSParser.ExpContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterFuncbody(@NotNull UMALSParser.FuncbodyContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitFuncbody(@NotNull UMALSParser.FuncbodyContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterStat(@NotNull UMALSParser.StatContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitStat(@NotNull UMALSParser.StatContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterOperatorPower(@NotNull UMALSParser.OperatorPowerContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitOperatorPower(@NotNull UMALSParser.OperatorPowerContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterOperatorOr(@NotNull UMALSParser.OperatorOrContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitOperatorOr(@NotNull UMALSParser.OperatorOrContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterVar(@NotNull UMALSParser.VarContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitVar(@NotNull UMALSParser.VarContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterOperatorAddSub(@NotNull UMALSParser.OperatorAddSubContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitOperatorAddSub(@NotNull UMALSParser.OperatorAddSubContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterOperatorMulDivMod(@NotNull UMALSParser.OperatorMulDivModContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitOperatorMulDivMod(@NotNull UMALSParser.OperatorMulDivModContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterLabel(@NotNull UMALSParser.LabelContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitLabel(@NotNull UMALSParser.LabelContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterFieldlist(@NotNull UMALSParser.FieldlistContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitFieldlist(@NotNull UMALSParser.FieldlistContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterOperatorStrcat(@NotNull UMALSParser.OperatorStrcatContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitOperatorStrcat(@NotNull UMALSParser.OperatorStrcatContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterArgs(@NotNull UMALSParser.ArgsContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitArgs(@NotNull UMALSParser.ArgsContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterField(@NotNull UMALSParser.FieldContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitField(@NotNull UMALSParser.FieldContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterVarSuffix(@NotNull UMALSParser.VarSuffixContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitVarSuffix(@NotNull UMALSParser.VarSuffixContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterTableconstructor(@NotNull UMALSParser.TableconstructorContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitTableconstructor(@NotNull UMALSParser.TableconstructorContext ctx) { }
+
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterEveryRule(@NotNull ParserRuleContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitEveryRule(@NotNull ParserRuleContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void visitTerminal(@NotNull TerminalNode node) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void visitErrorNode(@NotNull ErrorNode node) { }
+}
---
+++ Umals/src/umals/recognizer/UMALSListener.java
@@ -0,0 +1,351 @@
+// Generated from C:\Users\Declan Easton\Documents\Development\UMALS\Umals\src\UMALSGrammar\UMALS.g4 by ANTLR 4.4
+package umals.recognizer;
+import org.antlr.v4.runtime.misc.NotNull;
+import org.antlr.v4.runtime.tree.ParseTreeListener;
+
+/**
+ * This interface defines a complete listener for a parse tree produced by
+ * {@link UMALSParser}.
+ */
+public interface UMALSListener extends ParseTreeListener {
+	/**
+	 * Enter a parse tree produced by {@link UMALSParser#operatorUnary}.
+	 * @param ctx the parse tree
+	 */
+	void enterOperatorUnary(@NotNull UMALSParser.OperatorUnaryContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link UMALSParser#operatorUnary}.
+	 * @param ctx the parse tree
+	 */
+	void exitOperatorUnary(@NotNull UMALSParser.OperatorUnaryContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link UMALSParser#funcname}.
+	 * @param ctx the parse tree
+	 */
+	void enterFuncname(@NotNull UMALSParser.FuncnameContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link UMALSParser#funcname}.
+	 * @param ctx the parse tree
+	 */
+	void exitFuncname(@NotNull UMALSParser.FuncnameContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link UMALSParser#operatorAnd}.
+	 * @param ctx the parse tree
+	 */
+	void enterOperatorAnd(@NotNull UMALSParser.OperatorAndContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link UMALSParser#operatorAnd}.
+	 * @param ctx the parse tree
+	 */
+	void exitOperatorAnd(@NotNull UMALSParser.OperatorAndContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link UMALSParser#fieldsep}.
+	 * @param ctx the parse tree
+	 */
+	void enterFieldsep(@NotNull UMALSParser.FieldsepContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link UMALSParser#fieldsep}.
+	 * @param ctx the parse tree
+	 */
+	void exitFieldsep(@NotNull UMALSParser.FieldsepContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link UMALSParser#string}.
+	 * @param ctx the parse tree
+	 */
+	void enterString(@NotNull UMALSParser.StringContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link UMALSParser#string}.
+	 * @param ctx the parse tree
+	 */
+	void exitString(@NotNull UMALSParser.StringContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link UMALSParser#functioncall}.
+	 * @param ctx the parse tree
+	 */
+	void enterFunctioncall(@NotNull UMALSParser.FunctioncallContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link UMALSParser#functioncall}.
+	 * @param ctx the parse tree
+	 */
+	void exitFunctioncall(@NotNull UMALSParser.FunctioncallContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link UMALSParser#parlist}.
+	 * @param ctx the parse tree
+	 */
+	void enterParlist(@NotNull UMALSParser.ParlistContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link UMALSParser#parlist}.
+	 * @param ctx the parse tree
+	 */
+	void exitParlist(@NotNull UMALSParser.ParlistContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link UMALSParser#chunk}.
+	 * @param ctx the parse tree
+	 */
+	void enterChunk(@NotNull UMALSParser.ChunkContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link UMALSParser#chunk}.
+	 * @param ctx the parse tree
+	 */
+	void exitChunk(@NotNull UMALSParser.ChunkContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link UMALSParser#explist}.
+	 * @param ctx the parse tree
+	 */
+	void enterExplist(@NotNull UMALSParser.ExplistContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link UMALSParser#explist}.
+	 * @param ctx the parse tree
+	 */
+	void exitExplist(@NotNull UMALSParser.ExplistContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link UMALSParser#retstat}.
+	 * @param ctx the parse tree
+	 */
+	void enterRetstat(@NotNull UMALSParser.RetstatContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link UMALSParser#retstat}.
+	 * @param ctx the parse tree
+	 */
+	void exitRetstat(@NotNull UMALSParser.RetstatContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link UMALSParser#varOrExp}.
+	 * @param ctx the parse tree
+	 */
+	void enterVarOrExp(@NotNull UMALSParser.VarOrExpContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link UMALSParser#varOrExp}.
+	 * @param ctx the parse tree
+	 */
+	void exitVarOrExp(@NotNull UMALSParser.VarOrExpContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link UMALSParser#number}.
+	 * @param ctx the parse tree
+	 */
+	void enterNumber(@NotNull UMALSParser.NumberContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link UMALSParser#number}.
+	 * @param ctx the parse tree
+	 */
+	void exitNumber(@NotNull UMALSParser.NumberContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link UMALSParser#prefixexp}.
+	 * @param ctx the parse tree
+	 */
+	void enterPrefixexp(@NotNull UMALSParser.PrefixexpContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link UMALSParser#prefixexp}.
+	 * @param ctx the parse tree
+	 */
+	void exitPrefixexp(@NotNull UMALSParser.PrefixexpContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link UMALSParser#nameAndArgs}.
+	 * @param ctx the parse tree
+	 */
+	void enterNameAndArgs(@NotNull UMALSParser.NameAndArgsContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link UMALSParser#nameAndArgs}.
+	 * @param ctx the parse tree
+	 */
+	void exitNameAndArgs(@NotNull UMALSParser.NameAndArgsContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link UMALSParser#namelist}.
+	 * @param ctx the parse tree
+	 */
+	void enterNamelist(@NotNull UMALSParser.NamelistContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link UMALSParser#namelist}.
+	 * @param ctx the parse tree
+	 */
+	void exitNamelist(@NotNull UMALSParser.NamelistContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link UMALSParser#functiondef}.
+	 * @param ctx the parse tree
+	 */
+	void enterFunctiondef(@NotNull UMALSParser.FunctiondefContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link UMALSParser#functiondef}.
+	 * @param ctx the parse tree
+	 */
+	void exitFunctiondef(@NotNull UMALSParser.FunctiondefContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link UMALSParser#block}.
+	 * @param ctx the parse tree
+	 */
+	void enterBlock(@NotNull UMALSParser.BlockContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link UMALSParser#block}.
+	 * @param ctx the parse tree
+	 */
+	void exitBlock(@NotNull UMALSParser.BlockContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link UMALSParser#operatorComparison}.
+	 * @param ctx the parse tree
+	 */
+	void enterOperatorComparison(@NotNull UMALSParser.OperatorComparisonContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link UMALSParser#operatorComparison}.
+	 * @param ctx the parse tree
+	 */
+	void exitOperatorComparison(@NotNull UMALSParser.OperatorComparisonContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link UMALSParser#varlist}.
+	 * @param ctx the parse tree
+	 */
+	void enterVarlist(@NotNull UMALSParser.VarlistContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link UMALSParser#varlist}.
+	 * @param ctx the parse tree
+	 */
+	void exitVarlist(@NotNull UMALSParser.VarlistContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link UMALSParser#exp}.
+	 * @param ctx the parse tree
+	 */
+	void enterExp(@NotNull UMALSParser.ExpContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link UMALSParser#exp}.
+	 * @param ctx the parse tree
+	 */
+	void exitExp(@NotNull UMALSParser.ExpContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link UMALSParser#funcbody}.
+	 * @param ctx the parse tree
+	 */
+	void enterFuncbody(@NotNull UMALSParser.FuncbodyContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link UMALSParser#funcbody}.
+	 * @param ctx the parse tree
+	 */
+	void exitFuncbody(@NotNull UMALSParser.FuncbodyContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link UMALSParser#stat}.
+	 * @param ctx the parse tree
+	 */
+	void enterStat(@NotNull UMALSParser.StatContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link UMALSParser#stat}.
+	 * @param ctx the parse tree
+	 */
+	void exitStat(@NotNull UMALSParser.StatContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link UMALSParser#operatorPower}.
+	 * @param ctx the parse tree
+	 */
+	void enterOperatorPower(@NotNull UMALSParser.OperatorPowerContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link UMALSParser#operatorPower}.
+	 * @param ctx the parse tree
+	 */
+	void exitOperatorPower(@NotNull UMALSParser.OperatorPowerContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link UMALSParser#operatorOr}.
+	 * @param ctx the parse tree
+	 */
+	void enterOperatorOr(@NotNull UMALSParser.OperatorOrContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link UMALSParser#operatorOr}.
+	 * @param ctx the parse tree
+	 */
+	void exitOperatorOr(@NotNull UMALSParser.OperatorOrContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link UMALSParser#var}.
+	 * @param ctx the parse tree
+	 */
+	void enterVar(@NotNull UMALSParser.VarContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link UMALSParser#var}.
+	 * @param ctx the parse tree
+	 */
+	void exitVar(@NotNull UMALSParser.VarContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link UMALSParser#operatorAddSub}.
+	 * @param ctx the parse tree
+	 */
+	void enterOperatorAddSub(@NotNull UMALSParser.OperatorAddSubContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link UMALSParser#operatorAddSub}.
+	 * @param ctx the parse tree
+	 */
+	void exitOperatorAddSub(@NotNull UMALSParser.OperatorAddSubContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link UMALSParser#operatorMulDivMod}.
+	 * @param ctx the parse tree
+	 */
+	void enterOperatorMulDivMod(@NotNull UMALSParser.OperatorMulDivModContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link UMALSParser#operatorMulDivMod}.
+	 * @param ctx the parse tree
+	 */
+	void exitOperatorMulDivMod(@NotNull UMALSParser.OperatorMulDivModContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link UMALSParser#label}.
+	 * @param ctx the parse tree
+	 */
+	void enterLabel(@NotNull UMALSParser.LabelContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link UMALSParser#label}.
+	 * @param ctx the parse tree
+	 */
+	void exitLabel(@NotNull UMALSParser.LabelContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link UMALSParser#fieldlist}.
+	 * @param ctx the parse tree
+	 */
+	void enterFieldlist(@NotNull UMALSParser.FieldlistContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link UMALSParser#fieldlist}.
+	 * @param ctx the parse tree
+	 */
+	void exitFieldlist(@NotNull UMALSParser.FieldlistContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link UMALSParser#operatorStrcat}.
+	 * @param ctx the parse tree
+	 */
+	void enterOperatorStrcat(@NotNull UMALSParser.OperatorStrcatContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link UMALSParser#operatorStrcat}.
+	 * @param ctx the parse tree
+	 */
+	void exitOperatorStrcat(@NotNull UMALSParser.OperatorStrcatContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link UMALSParser#args}.
+	 * @param ctx the parse tree
+	 */
+	void enterArgs(@NotNull UMALSParser.ArgsContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link UMALSParser#args}.
+	 * @param ctx the parse tree
+	 */
+	void exitArgs(@NotNull UMALSParser.ArgsContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link UMALSParser#field}.
+	 * @param ctx the parse tree
+	 */
+	void enterField(@NotNull UMALSParser.FieldContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link UMALSParser#field}.
+	 * @param ctx the parse tree
+	 */
+	void exitField(@NotNull UMALSParser.FieldContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link UMALSParser#varSuffix}.
+	 * @param ctx the parse tree
+	 */
+	void enterVarSuffix(@NotNull UMALSParser.VarSuffixContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link UMALSParser#varSuffix}.
+	 * @param ctx the parse tree
+	 */
+	void exitVarSuffix(@NotNull UMALSParser.VarSuffixContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link UMALSParser#tableconstructor}.
+	 * @param ctx the parse tree
+	 */
+	void enterTableconstructor(@NotNull UMALSParser.TableconstructorContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link UMALSParser#tableconstructor}.
+	 * @param ctx the parse tree
+	 */
+	void exitTableconstructor(@NotNull UMALSParser.TableconstructorContext ctx);
+}
